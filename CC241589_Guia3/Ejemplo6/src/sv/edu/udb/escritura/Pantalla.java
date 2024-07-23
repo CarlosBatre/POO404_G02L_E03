f9200d17fab27bef5c5dc03ab78a7ce31bde76ad
Rexerus0 <CC241589@alumno.udb.edu.sv>
2024-07-23 15:11:08 -0600

    Delete CC241589_Guia3 directory

--- CC241589_Guia3/Ejemplo6/src/sv/edu/udb/escritura/Pantalla.java
+++
@@ -1,9 +0,0 @@
-package sv.edu.udb.escritura; //Paquete en el que se encuentra la clase Pantalla
-public class Pantalla {
-    public void sinSalto(String s) {
-        System.out.print(s);
-    }
-    public void conSalto(String s) {
-        System.out.println(s);
-    }
-}
